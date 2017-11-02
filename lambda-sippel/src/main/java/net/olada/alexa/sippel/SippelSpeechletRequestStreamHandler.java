062b55f41e486cf56c3cd945373d90c927ff4e57
David <david.olah@gmail.com>
2017-11-02 22:16:44 +0100

    Application-ID hinzugefügt

--- lambda-sippel/src/main/java/net/olada/alexa/sippel/SippelSpeechletRequestStreamHandler.java
+++ lambda-sippel/src/main/java/net/olada/alexa/sippel/SippelSpeechletRequestStreamHandler.java
@@ -4,16 +4,23 @@ package net.olada.alexa.sippel;
  * Created by David Olah on 02.11.2017.
  */
 
-import com.amazon.speech.speechlet.SpeechletV2;
 import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Wird von AWS Lambda instanziiert
  */
-public class SippelSpeechletRequestStreamHandler extends SpeechletRequestStreamHandler{
-    public SippelSpeechletRequestStreamHandler(SpeechletV2 speechlet, Set<String> supportedApplicationIds) {
-        super(speechlet, supportedApplicationIds);
+public class SippelSpeechletRequestStreamHandler extends SpeechletRequestStreamHandler {
+    private static final Set<String> supportedApplicationIds;
+
+    static {
+        supportedApplicationIds = new HashSet<String>();
+        supportedApplicationIds.add("amzn1.ask.skill.8adb2663-e236-4397-91f9-4500ed607b2b");
+    }
+
+    public SippelSpeechletRequestStreamHandler() {
+        super(new SippelSpeechlet(), supportedApplicationIds);
     }
 }
